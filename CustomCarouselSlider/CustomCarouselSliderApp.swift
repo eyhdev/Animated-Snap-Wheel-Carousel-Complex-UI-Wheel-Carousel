30791058c6706050f1a83391ae3139f4771e8c67
Dev Light <106925514+developerlight@users.noreply.github.com>
2022-07-28 13:48:37 +0300

    Initial Commit

---
+++ CustomCarouselSlider/CustomCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouselSliderApp.swift
+//  CustomCarouselSlider
+//
+//  Created by Developer Light on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
